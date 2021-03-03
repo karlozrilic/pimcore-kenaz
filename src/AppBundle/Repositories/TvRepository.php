5ebc0cad804fd425e4dcf7fc0d563f8350c67539
karlozrilic <kazrile@gmail.com>
2021-03-03 11:30:49 +0100

    added aside

--- src/AppBundle/Repositories/TvRepository.php
+++ src/AppBundle/Repositories/TvRepository.php
@@ -12,8 +12,16 @@ class TvRepository
      */
     public function getTopVideo() {
         $videos = [
-            'https://www.youtube.com/embed/2UoovdljmX8',
-            'https://www.youtube.com/embed/HR1SLEPTp78'
+            [
+                'link' => 'https://www.youtube.com/embed/2UoovdljmX8',
+                'title' => 'FOI - Tjedan karijera 2020 I Factory.hr',
+                'date' => '2020-08-12 00:00:00'
+            ], 
+            [
+                'link' => 'https://www.youtube.com/embed/HR1SLEPTp78',
+                'title' => 'FOI - Tjedan karijera 2020 I Upoznaj Factory studente! I Factory.hr',
+                'date' => '2020-08-13 00:00:00'
+            ]
         ];
 
         return $videos[array_rand($videos, 1)];
